592ec5f4ddc97b21a7a59292672a4a21db957f20
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2020-03-25 00:48:43 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,6 @@
 INSERT INTO bear (name) VALUES ("Mr. Chocolate");
+INSERT INTO bear (name) VALUES ("Mr. Chocolate");
+INSERT INTO bear (name) VALUES ("Mr. Chocolate");
+INSERT INTO bear (name) VALUES ("Mr. Chocolate");
+INSERT INTO bear (name) VALUES ("Mr. Chocolate");
+INSERT INTO bear (name) VALUES ("Mr. Chocolate");
